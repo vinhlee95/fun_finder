1ba8e523ee223d0382e4c457b3c8cab48510e9ef
Vinh Le <lethanhvinh95@gmail.com>
2024-08-17 10:40:19 +0300

    add tools

--- db.py
+++ db.py
@@ -1,4 +1,7 @@
 import psycopg2
+from langchain.tools import tool
+from pydantic.v1 import BaseModel
+
 
 conn = psycopg2.connect(
   dbname="tennis_reservation",
@@ -7,12 +10,43 @@ conn = psycopg2.connect(
   port="5432",
 )
 
-cur = conn.cursor()
+def list_tables():
+  cur = conn.cursor()
+  cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema='public';")
+  tables = cur.fetchall()
+  return tables
 
-# Test
-cur.execute("select * from available_reservation where date = '2024-08-18' and court_id = 'Court 2'")
+class RunQueryToolArgSchema(BaseModel):
+  query: str
 
-records = cur.fetchall()
+@tool(
+  args_schema=RunQueryToolArgSchema
+)
+def run_query_tool(query):
+  """
+  Given a query string, run the query and return the result.
+  """
+  try:
+    c = conn.cursor()
+    c.execute(query)
+    return c.fetchall()
+  except Exception as e:
+    return f"An exception occurred {str(e)}"
+  
 
-for record in records:
-  print(record)
+class DescribeTableSchema(BaseModel):
+  tables: list[str]
+
+@tool(
+  args_schema=DescribeTableSchema,
+)
+def describe_tables_tool(tables: list[str]):
+  """
+  Given a list of table names, return SQL schema of these tables
+  """
+  cur = conn.cursor()
+  result = []
+  for table in tables:
+    cur.execute(f"SELECT column_name, data_type FROM information_schema.columns WHERE table_name = '{table}';")
+    result.append(cur.fetchall())
+  return result
--- main.py
+++ main.py
@@ -1,1 +1,47 @@
-print("hello world")
+from langchain_openai import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder
+from langchain.schema import SystemMessage
+from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
+
+from dotenv import load_dotenv
+
+from db import describe_tables_tool, list_tables, run_query_tool
+
+load_dotenv()
+
+llm = ChatOpenAI()
+
+
+tables = list_tables()
+
+prompt = ChatPromptTemplate(
+  messages=[
+    SystemMessage(content=
+      f"You are an AI having access to a PostgreSQL database having following tables: {tables}. \n"
+      "Do not make any assumption about the tables. \n"
+      "Instead always try to use 'describe_tables_tool' to get schema of relevant tables \n"
+      "before you try to figure out the query."
+    ),
+    HumanMessagePromptTemplate.from_template("{input}"),
+    # agent_scratchpad is specific, kinda a simple form of memorising 
+    # the output of the tool in the chain
+    MessagesPlaceholder(variable_name="agent_scratchpad")
+  ],
+  input_variables=[],
+)
+
+tools = [run_query_tool, describe_tables_tool]
+
+agent = OpenAIFunctionsAgent(
+  llm=llm,
+  prompt=prompt,
+  tools=tools,
+)
+
+agent_executor = AgentExecutor(
+  agent=agent,
+  tools=tools,
+  verbose=True,
+)
+
+agent_executor.run("Are there any available hours on Court 2 on 2024-08-18? Give me the results with this format: court_id - hour")
